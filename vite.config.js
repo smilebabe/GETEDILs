30a465f4315166977ce42a779df27b78f1f4a44b
smilebabe <165559327+smilebabe@users.noreply.github.com>
2026-04-04 11:19:58 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: './', // Ensures Vite looks in the current folder for index.html
+  root: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
