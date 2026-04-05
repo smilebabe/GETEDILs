2e335ce5b9938bda73bcec52f7657b596a87f3f3
smilebabe <165559327+smilebabe@users.noreply.github.com>
2026-04-05 06:49:46 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: './',
-  resolve: {
-    alias: {
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@core': path.resolve(__dirname, 'src/core'),
-      '@lib': path.resolve(__dirname, 'src/lib'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@store': path.resolve(__dirname, 'src/store'),
-      '@styles': path.resolve(__dirname, 'src/styles'),
-      '@routes': path.resolve(__dirname, 'src/routes'),
-    },
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
-  },
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      external: []
-    }
+    sourcemap: true
   },
   server: {
     port: 3000,
